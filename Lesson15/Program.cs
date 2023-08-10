db6556b7c3d557ae9c70a7064925f065ed784e92
ievbondariev <iev.bondariev@gmail.com>
2023-08-10 17:08:25 +0200

    added method

--- Lesson15/Program.cs
+++ Lesson15/Program.cs
@@ -49,20 +49,20 @@
     {
         static void Main(string[] args)
         {
-            Animal cat = new Cat();
-            cat.Method();
-            //Animal animal = new Animal();
-            //animal.Name = "Test";
-            //animal.SayHi();
-            //Console.WriteLine(animal.ToString());
             //Animal cat = new Cat();
-            //Animal dog = new Dog();
-            //Cat cat2 = (Cat)cat;
-            ////Dog dog1 = (Dog)cat;
-            //Dog dog2 = cat as Dog;
-            //cat2.Breed = "catttt";
-            //cat.SayHi();
-            //dog.SayHi();
+            //cat.Method();
+            Animal animal = new Animal();
+            animal.Name = "Test";
+            animal.SayHi();
+            Console.WriteLine(animal.ToString());
+            Animal cat = new Cat();
+            Animal dog = new Dog();
+            Cat cat2 = (Cat)cat;
+            //Dog dog1 = (Dog)cat;
+            Dog dog2 = cat as Dog;
+            cat2.Breed = "catttt";
+            cat.SayHi();
+            dog.SayHi();
         }
     }
 }
